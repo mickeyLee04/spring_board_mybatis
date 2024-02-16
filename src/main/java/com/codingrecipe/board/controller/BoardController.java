cd56ab551a1bedf5a2b58f515348aae6877ee357
mickeyLee04 <micky040404@gmail.com>
2024-02-16 13:29:16 +0900

    java spring mybatis - third upload

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.board.controller;
 
 import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.dto.BoardFileDTO;
 import com.codingrecipe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -22,7 +24,7 @@ public class BoardController {
     }
 
     @PostMapping("/save")
-    public String save(BoardDTO boardDTO) {
+    public String save(BoardDTO boardDTO) throws IOException {
         System.out.println(boardDTO);
         boardService.save(boardDTO);
         return "redirect:/list";
@@ -40,6 +42,10 @@ public class BoardController {
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
+        if(boardDTO.getFileAttached() == 1){
+            BoardFileDTO boardFileDTO = boardService.findFile(id);
+            model.addAttribute("boardFile", boardFileDTO);
+        }
         return "detail";
     }
 
--- src/main/java/com/codingrecipe/board/repository/BoardRepository.java
+++ src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.board.repository;
 
 import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.dto.BoardFileDTO;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,8 +13,9 @@ import java.util.List;
 public class BoardRepository {
     private final SqlSessionTemplate sql;
 
-    public void save(BoardDTO boardDTO) {
+    public BoardDTO save(BoardDTO boardDTO) {
         sql.insert("Board.save", boardDTO);
+        return boardDTO;
     }
 
     public List<BoardDTO> findAll() {
@@ -34,5 +36,13 @@ public class BoardRepository {
 
     public void delete(Long id) {
         sql.delete("Board.delete", id);
+    }
+
+    public void saveFile(BoardFileDTO boardFileDTO) {
+        sql.insert("Board.saveFile", boardFileDTO);
+    }
+
+    public BoardFileDTO findFile(Long id) {
+        return sql.selectOne("Board.findFile", id);
     }
 }
