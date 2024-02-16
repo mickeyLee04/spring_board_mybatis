006c1475c288782874f7f6887409dfe36280bd52
mickeyLee04 <micky040404@gmail.com>
2024-02-16 10:31:03 +0900

    java spring mybatis - first upload

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,45 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(BoardDTO boardDTO) {
+        System.out.println(boardDTO);
+        boardService.save(boardDTO);
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+}
